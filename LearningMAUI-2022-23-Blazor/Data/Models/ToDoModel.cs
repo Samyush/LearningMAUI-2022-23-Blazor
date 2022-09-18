dcfd9f21b27246bef3e97c0bc92ae756fb2abbfb
samyush <samyush.maharjan@innovatetech.co>
2022-09-18 22:40:49 +0545

    completion with csv file write

---
+++ LearningMAUI-2022-23-Blazor/Data/Models/ToDoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMAUI_2022_23_Blazor.Data.Models
+{
+    public class ToDoModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string TaskDate { get; set; }
+        public string TaskType { get; set; }
+
+    }
+}
---
+++ LearningMAUI-2022-23-Blazor/Data/WriteToCsv.cs
@@ -0,0 +1,40 @@
+﻿using LearningMAUI_2022_23_Blazor.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMAUI_2022_23_Blazor.Data
+{
+    public class WriteToCsv
+    {
+        public static void WriteDataToCsv(ToDoModel data, string path)
+        {
+            var sw = new StreamWriter(path);
+
+            sw.WriteLine("TaskName,TaskDescription,TaskDate,TaskType");
+
+            //foreach (var item in data)
+            //{
+
+                sw.WriteLine($"{data.Name},{data.Description},{data.TaskDate},{data.TaskType},");
+
+            //}
+            sw.Close();
+            Console.WriteLine("Success");
+        }
+
+        public static void Wr2(ToDoModel data, string path)
+        {
+            var csv = new StringBuilder();
+
+            //Suggestion made by KyleMit
+            var newLine = string.Format("{0},{1},{2},{3}", data.Name, data.Description, data.TaskDate, data.TaskType);
+            csv.AppendLine(newLine);
+
+            //after your loop
+            File.AppendAllText(path, csv.ToString());
+        }
+    }
+}
